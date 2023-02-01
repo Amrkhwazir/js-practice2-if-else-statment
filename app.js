143b4d94753bee9160be01b4ca652af6f6dbec6d
Amirkhan567 <amrkh5678@gmail.com>
2023-02-01 14:32:54 +0500

    practice 2 accomplished

---
+++ app.js
@@ -0,0 +1,52 @@
+// var firstName = prompt("Enter your firstName")
+// var lastName = prompt("Enter your lastName")
+// var age = prompt("Enter your age")
+// alert ("my name is" + " " + firstName +  " " + lastName + " " + "my age is" + " " + age);
+
+// if(age == "18"){
+//     alert ("bike chalana allow hai");
+// }else{
+//     alert ("bike chalana allow nahin hai");
+
+// }
+
+// if(age >= "18"){
+//     alert ("bike chalana allow hai");
+// }else{
+//     alert ("bike chalana allow nahin hai");
+
+// }
+
+// if(age != "18"){
+//     alert ("bike chalana allow hai");
+// }else{
+//     alert ("bike chalana allow nahin hai");
+
+// }
+
+var days = prompt("Enter week days")
+
+if(days === "monday"){
+    alert("Gosht")
+}else if (days === "tuesday"){
+    alert("mutton")
+
+}else if (days === "wednesday"){
+    alert("sabzi")
+
+}else if (days === "thursday"){
+    alert("chicken karahi")
+
+}else if (days === "friday"){
+    alert("biryani")
+
+}else if (days === "saturday"){
+    alert("daal chawal")
+
+}else if (days === "sunday"){
+    alert("holiday")
+
+}
+
+
+
